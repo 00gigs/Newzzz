017e9996beb12d2e3e0c035ae5cee8161c0a3667
00gigs <1onlinegigs@gmail.com>
2024-02-15 01:49:42 -0500

    new

---
+++ src/app/firecon.tsx
@@ -0,0 +1,17 @@
+// Initialize Firebase
+    
+        // Initialize authentication
+
+import { initializeApp } from "firebase/app";
+ const app = initializeApp({
+    apiKey:'AIzaSyB5dqniGqJ46tafGeoHmpAkUD1XtRSH1mQ',
+    authDomain: 'newz-50ae6.firebaseapp.com',
+    projectId: 'newz-50ae6',
+    storageBucket: 'newz-50ae6.appspot.com',
+    messagingSenderId:'839009054216' ,
+    appId:'1:839009054216:web:703718165016af82a16bbd',
+    measurementId:'G-LR13Y1B2ED' 
+  });
+
+  
+export default app
